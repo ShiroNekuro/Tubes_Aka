0adb682b81d988840442565f3c895b55ba0dd469
ShiroNekuro <73917427+ShiroNekuro@users.noreply.github.com>
2024-12-25 22:36:36 +0700

    Update mmrsort.go

--- mmrsort.go
+++ mmrsort.go
@@ -7,7 +7,7 @@ type Player struct {
 	MMR  int
 }
 
-// Sorting MMR
+// Sorting MMR dan menampilkan MMR
 func bubbleSort(players []Player) {
 	n := len(players)
 	for i := 0; i < n-1; i++ {
@@ -18,8 +18,14 @@ func bubbleSort(players []Player) {
 			}
 		}
 	}
+	fmt.Println("\nRanked Players:")
+	for rank, player := range players {
+		fmt.Printf("%d. %s: %d\n", rank+1, player.Name, player.MMR)
+	}
+	option(players)
 }
 
+//Inisialisasi Data player
 func main() {
 	players := []Player{
 		{Name: "Alice", MMR: 1200},
@@ -27,14 +33,52 @@ func main() {
 		{Name: "Charlie", MMR: 1100},
 		{Name: "Diana", MMR: 1250},
 	}
-  option(players)
+	option(players)
 }
 
-func option(players [])
-	// eksekusi sort
-	/*bubbleSort(players)
-
-	fmt.Println("\nRanked Players:")
-	for rank, player := range players {
-		fmt.Printf("%d. %s: %d\n", rank+1, player.Name, player.MMR)
+//Menu
+func option(players []Player){
+	var menu int
+	fmt.Println("===========Menu==========")
+	fmt.Println("1. Tampilkan data")
+	fmt.Println("2. Simulasi Match")
+	fmt.Println("==========================")
+	fmt.Scan(&menu)
+	switch menu{
+		case 1:
+			bubbleSort(players)
+		case 2:
+			simulate(players)
 	}
+}
+
+//simulasi match
+func simulate(players []Player){
+	var name string
+	var winlose string
+	fmt.Println("Masukkan nama player: ")
+	fmt.Scan(&name)
+	id := findPlayer(players, name)
+	if id == -1 {
+		fmt.Println("Player tidak ditemukan")
+		option(players)
+	}
+	fmt.Println("menang/kalah")
+	fmt.Scan(&winlose)
+	if winlose == "menang" {
+		players[id].MMR += 50
+	} else if winlose == "kalah" {
+		players[id].MMR -= 50
+	}
+	option(players)
+}
+
+//function untuk mencari player dan mereturn index
+func findPlayer(players []Player, name string) int{
+	for i, player := range players {
+		if player.Name == name {
+			return i
+		}
+	}
+	return -1
+}
